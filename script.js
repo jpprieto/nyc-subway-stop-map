28e3ce81772e23a91b175515fda94daf8d7e1e8a
jpprieto <misticientist@gmail.com>
2017-01-10 23:36:09 +0000

    first commit

--- script.js
+++ script.js
@@ -2,30 +2,33 @@ $( document ).ready(function()
     {
         $("#submitButton").click(function()
         {
-            // 1. Get the value of the subway stop IDs from the input field
-            // HINT: Use ClickHandlers
+            // // 1. Get the value of the subway stop IDs from the input field
+            // // HINT: Use ClickHandlers
+            // var st1 = $("#st1").val();
+            // var st2 = $("#st2").val();
+            // var st3 = $("#st3").val();
+    
             
             
-            
-            // 2. Make API call to Subway API to get the Stop Info - (mtaapi.herokuapp.com/stop)
-            //    Save the stop name
-            //    Save the latitude and longitude in the format lat,lon (example: 40.752769,-73.979189)
-            //    Repeat this exercise 3 times to get name and coordinates for Stop 1, 2 and 3
-            var stop1Obj = $.getJSON(
-                    "URL",
-                    function(response) {
-                        return response;
-                    }
-                );
-            var stop1Name = "...";
-            var stop1Coordinates = "...";
-                // Repeat this for Stop 2 and 3
+            // // 2. Make API call to Subway API to get the Stop Info - (mtaapi.herokuapp.com/stop)
+            // //    Save the stop name
+            // //    Save the latitude and longitude in the format lat,lon (example: 40.752769,-73.979189)
+            // //    Repeat this exercise 3 times to get name and coordinates for Stop 1, 2 and 3
+            // var stop1Obj = $.getJSON(
+            //         "mtaapi.herokuapp.com/stop?id=" + st1,
+            //         function(response) {
+            //             return response;
+            //         }
+            //     );
+            // var stop1Name = result.name;
+            // var stop1Coordinates = result.lat;
+            //     // Repeat this for Stop 2 and 3
 
             
-            // 3. Display the stop names on the HTML
-            // HINT: Use Javascript Object Dot Notation
-            $("#stop1").text("....");
-                // Repeat this for Stop 2 and 3
+            // // 3. Display the stop names on the HTML
+            // // HINT: Use Javascript Object Dot Notation
+            // $("#stop1").text("....");
+            //     // Repeat this for Stop 2 and 3
                 
                 
                 
@@ -40,11 +43,11 @@ $( document ).ready(function()
             //    &markers=color:blue%7Clabel:B%7C40.754222,-73.984569
             //    &markers=color:green%7Clabel:C%7C40.758663,-73.981329
             //    &key=YOUR_API_KEY
-            var map_url = "";
+            var map_url = "https://maps.googleapis.com/maps/api/staticmap?zoom=16&size=2000x2000&markers=color:red%7Clabel:A%7C40.752769,-73.979189&markers=color:blue%7Clabel:B%7C40.754222,-73.984569&markers=color:green%7Clabel:C%7C40.758663,-73.981329&key=AIzaSyDNANuHlcA_sZOsF0RZUflUujnwRdhDpIQ";
             
             // 5. Display the map on the HTML
                 $("#map").append("<img src=" + map_url + ">");
-
+         
         });
                     
     });
